9324a2662ad7332b6a4e8ab30aa6d152feda6a74
Даниил Прыгунов <idanil430@gmail.com>
2022-11-28 19:51:00 +0200

    Created ticket model

--- TicketManagmentService/src/main/java/com/fintech/ticket/model/Flight.java
+++ TicketManagmentService/src/main/java/com/fintech/ticket/model/Flight.java
@@ -1,17 +1,36 @@
 package com.fintech.ticket.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.annotations.GeneratorType;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
-@Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "flight")
 public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "from_city", nullable = false)
     private String fromCity;
+    @Column(name = "to_city", nullable = false)
     private String toCity;
+    @Column(name = "train_start_time")
     private LocalDateTime trainStartTime;
+    @Column(name = "available_tickets", nullable = false)
     private Integer availableTickets;
+    @OneToMany(mappedBy = "flight_plan",cascade = CascadeType.REMOVE)
+    private List<Ticket> tickets;
+
+    public Integer getAvailableTickets() {
+        return availableTickets = tickets.size();
+    }
 }
---
+++ TicketManagmentService/src/main/java/com/fintech/ticket/model/Ticket.java
@@ -0,0 +1,24 @@
+package com.fintech.ticket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_plan_id")
+    private Flight flight_plan;
+}
