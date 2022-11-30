29f2178dbf88c249bd14f9e3733a7fe828c738af
Даниил Прыгунов <idanil430@gmail.com>
2022-11-30 14:29:01 +0200

    Created repository interface for payment

---
+++ OrderService/src/main/java/com/fintech/order/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.fintech.order.repository;
+
+import com.fintech.order.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
+}
